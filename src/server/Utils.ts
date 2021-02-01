0be3a488ab1eac963b2c963a367bdec935c117ac
pDunCanCode <chesiredarling@gmail.com>
2021-01-31 17:46:34 -0800

    seems to be working -

--- src/server/Utils.ts
+++ src/server/Utils.ts
@@ -1,23 +1,22 @@
-import { UrlWithParsedQuery } from 'url';
-const URL = require('url')
+import { parse, UrlWithParsedQuery } from "url";
 
 export class Utils {
-
-
-    public static getUrlBasePath(url: string | undefined): string {
-        if (url) {
-            const parsedUrl = new URL(url);
-            return parsedUrl.pathname!.split('/')[1];
-        } else {
-            return '';
-        }
+  public static getUrlBasePath(url: string | undefined): string {
+    if (url) {
+      const parsedUrl = parse(url);
+      return parsedUrl.pathname!.split("/")[1];
+    } else {
+      return "";
     }
+  }
 
-    public static getUrlParameters(url: string | undefined): UrlWithParsedQuery | undefined {
-        if (url) {
-            return new URL(url, true);
-        } else {
-            return undefined;
-        }
+  public static getUrlParameters(
+    url: string | undefined
+  ): UrlWithParsedQuery | undefined {
+    if (url) {
+      return parse(url, true);
+    } else {
+      return undefined;
     }
+  }
 }
